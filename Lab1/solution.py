a23eac0a680ea7fe6ddea4bb36d63e63500ebdfc
v.rusnak <vladrusnak2@outlook.com>
2023-12-17 13:04:09 +0200

    lab 1 refactored

--- Lab1/solution.py
+++ Lab1/solution.py
@@ -1,57 +1,60 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lower, regexp_replace, split, size, expr, concat_ws
+from pyspark.sql.functions import col, regexp_replace, split, size, expr, concat_ws, udf
+from pyspark.sql.types import StringType
 from pyspark.ml.feature import NGram, Tokenizer
 from pyspark.ml import Pipeline
-import re
 
-# Create a Spark session
-spark = SparkSession.builder.appName("GitHubNGrams").getOrCreate()
+def create_spark_session():
+    """Create and return a Spark session."""
+    return SparkSession.builder.appName("GitHubNGrams").getOrCreate()
 
-# Load the JSON data into a DataFrame
-json_data = spark.read.json("10K.github.jsonl")
+def load_json_data(spark, file_path):
+    """Load JSON data into a DataFrame."""
+    return spark.read.json(file_path)
 
-# Filter data for PushEvent type
-push_events = json_data.filter(col("type") == "PushEvent")
-
-# Define a function to extract and preprocess commit messages
-def process_commit_messages(commit_messages):
+def preprocess_commit_messages(commit_messages):
+    """Preprocess commit messages."""
     # Convert to lowercase and remove punctuation and underscores
-    cleaned_messages = [re.sub(r'\W+', ' ', msg.lower()) for msg in commit_messages]
-    return ' '.join(cleaned_messages)
+    return commit_messages.lower().translate(str.maketrans('', '', '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'))
 
-# UDF for processing commit messages
-process_commit_messages_udf = spark.udf.register("process_commit_messages", process_commit_messages)
+def process_data(df):
+    """Process the DataFrame and apply transformations."""
+    preprocess_udf = udf(preprocess_commit_messages, StringType())
+    
+    # Tokenize and apply NGram transformation
+    tokenizer = Tokenizer(inputCol="processed_commits", outputCol="tokenized_words")
+    ngram = NGram(n=3, inputCol=tokenizer.getOutputCol(), outputCol="ngrams_result")
+    pipeline = Pipeline(stages=[tokenizer, ngram])
+    model = pipeline.fit(df)
+    result = model.transform(df)
 
-# Apply transformations to the DataFrame
-processed_data = push_events.withColumn(
-    "processed_commits",
-    process_commit_messages_udf(col("payload.commits.message"))
-)
+    # Extract only the first five words from the n-grams
+    result = result.withColumn(
+        "first_five_words",
+        expr("slice(ngrams_result, 1, 5)")
+    )
 
-# Tokenize and apply NGram transformation
-tokenizer = Tokenizer(inputCol="processed_commits", outputCol="tokenized_words")
-ngram = NGram(n=3, inputCol=tokenizer.getOutputCol(), outputCol="ngrams_result")
-pipeline = Pipeline(stages=[tokenizer, ngram])
-model = pipeline.fit(processed_data)
-result = model.transform(processed_data)
+    # Convert the array of strings to a single string
+    result = result.withColumn("first_five_words", concat_ws(", ", col("first_five_words")))
 
-# Extract only the first five words from the n-grams
-result = result.withColumn(
-    "first_five_words",
-    expr("slice(ngrams_result, 1, case when size(ngrams_result) >= 5 then 5 else size(ngrams_result) end)")
-)
+    return result
 
-# Handle cases where 1-2 words are present
-result = result.withColumn(
-    "first_five_words",
-    expr("case when size(tokenized_words) <= 2 then tokenized_words else first_five_words end")
-)
+def main():
+    spark = create_spark_session()
 
-# Convert the array of strings to a single string
-result = result.withColumn("first_five_words", concat_ws(", ", col("first_five_words")))
+    try:
+        json_data = load_json_data(spark, "10K.github.jsonl")
+        push_events = json_data.filter(col("type") == "PushEvent")
+        processed_data = push_events.withColumn(
+            "processed_commits",
+            preprocess_commit_messages(col("payload.commits.message"))
+        )
+        result = process_data(processed_data)
+        result.select("actor.display_login", "first_five_words").coalesce(1).write.csv("output.csv", header=True, mode="overwrite")
+    except Exception as e:
+        print(f"Error processing data: {e}")
+    finally:
+        spark.stop()
 
-# Save the result to a CSV file
-result.select("actor.display_login", "first_five_words").coalesce(1).write.csv("output.csv", header=True, mode="overwrite")
-
-# Stop the Spark session
-spark.stop()
+if __name__ == "__main__":
+    main()
